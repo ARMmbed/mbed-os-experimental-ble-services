8a752bed8cb23c94431091300bce41589ae2a099
Vincent Coubard <vincent.coubard@arm.com>
2020-10-28 19:09:06 +0000

    Add mock of the GattServer

--- tests/source/dummy_service_test.cpp
+++ tests/source/dummy_service_test.cpp
@@ -1,14 +1,22 @@
 // Dummy test to setup CMake 
 
+#include "mocks/GattServerMock.h"
 #include "gtest/gtest.h"
 
 #include "mbed-ble-services/dummy_service.h"
 
+#include "ble/BLE.h"
 
 // Tests factorial of 0.
 TEST(FactorialTest, HandlesZeroInput) {
+    // test instantiation of stubs and mocks
+    auto &ble = ble::BLE::Instance();
+    ble.setGattServer(nullptr);
+    GattServerMock mock;
+
   EXPECT_EQ(Factorial(0), 1);
 }
+
 
 // Tests factorial of positive numbers.
 TEST(FactorialTest, HandlesPositiveInput) {
@@ -17,3 +25,5 @@ TEST(FactorialTest, HandlesPositiveInput) {
   EXPECT_EQ(Factorial(3), 6);
   EXPECT_EQ(Factorial(8), 40320);
 }
+
+
