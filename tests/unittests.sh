274e60e4be43921afdb9342292c8b5704399e378
Paul Szczepanek <paul.szczepanek@arm.com>
2021-02-04 13:36:41 +0000

    add workflow and action

--- tests/unittests.sh
+++ tests/unittests.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 set -e
 
+# set pwd to script location
+cd "$( dirname "$0" )"
+
 # we need stubs from mbed-os
 if [ -d "mbed-os" ]
 then
     echo "Using existing mbed-os"
 else
-    git clone https://github.com/ARMmbed/mbed-os.git
+    #git clone https://github.com/ARMmbed/mbed-os.git
+    # until it's not merged we use my branch
+    git clone --depth 1 https://github.com/paul-szczepanek-arm/mbed-os.git -b cmake-tests
 fi
 
 cmake -S . -B cmake_build -GNinja
