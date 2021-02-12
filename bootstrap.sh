e9e61fb632da0a18f21f737ca64d21e619d4e713
Vincent Coubard <vincent.coubard@arm.com>
2021-02-12 16:04:11 +0000

    Add VSCode environment (#36)
    * Add default configuration for vscode.
    
    * VSCode: Set Dark+ mode to enhance visibility.
    
    * VSCode: ignore build folder
    
    * VSCode: do not hardcode compiler path and let the extension find it.
    
    * VSCode: Add a task to bootstrap the environment.
    
    * Add instructions to start with VScode.
    
    * Update doc/DEVELOPMENT.md
    
    Co-authored-by: Paul Szczepanek <paul.szczepanek@arm.com>
    
    * Update doc/DEVELOPMENT.md
    
    Co-authored-by: Paul Szczepanek <paul.szczepanek@arm.com>
    
    * Update doc/DEVELOPMENT.md
    
    Co-authored-by: Paul Szczepanek <paul.szczepanek@arm.com>
    
    * Update doc/DEVELOPMENT.md
    
    Co-authored-by: Paul Szczepanek <paul.szczepanek@arm.com>
    
    * Update doc/DEVELOPMENT.md
    
    Co-authored-by: Paul Szczepanek <paul.szczepanek@arm.com>
    
    * Move minimum requirement section at the top of DEVELOPMENT.md
    
    * Call bootstrap from unittest.sh
    
    * Update mbed-os branch in bootstrap.sh
    
    * Update mock deletion in CurrentTime service test
    
    * Fix rebase issues
    
    * Remove git and test extensions from container.
    
    Default git support from VSCode and CMake is enough.
    
    * Run bootstrap after container creation.
    
    * Update postCreateCommand
    
    * Update development instructions:
    - Image of the CMake toolbar
    - Remove the bootstrap part in container doc, this is now done
    automaticaly.
    - Added reference to docker configuration for non root user.
    
    * Update doc/DEVELOPMENT.md
    
    Co-authored-by: Abbas Bracken Ziad <abbas.brackenziad@arm.com>
    
    * Update doc/DEVELOPMENT.md
    
    Co-authored-by: Abbas Bracken Ziad <abbas.brackenziad@arm.com>
    
    * Update doc/DEVELOPMENT.md
    
    Co-authored-by: Abbas Bracken Ziad <abbas.brackenziad@arm.com>
    
    * Update doc/DEVELOPMENT.md
    
    Co-authored-by: Abbas Bracken Ziad <abbas.brackenziad@arm.com>
    
    * Update .devcontainer/devcontainer.json
    
    Co-authored-by: Abbas Bracken Ziad <abbas.brackenziad@arm.com>
    
    * Add numbered bullet points in getting started section.
    
    * Update doc/DEVELOPMENT.md
    
    Co-authored-by: Abbas Bracken Ziad <abbas.brackenziad@arm.com>
    
    * Update doc/DEVELOPMENT.md
    
    Co-authored-by: Abbas Bracken Ziad <abbas.brackenziad@arm.com>
    
    Co-authored-by: Paul Szczepanek <paul.szczepanek@arm.com>
    Co-authored-by: Abbas Bracken Ziad <abbas.brackenziad@arm.com>

---
+++ bootstrap.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+# set pwd to script location
+cd "$( dirname "$0" )"
+
+# enter the test folder 
+cd ./tests
+
+# we need stubs from mbed-os
+if [ -d "mbed-os" ]
+then
+    echo "Using existing mbed-os"
+else
+    #git clone https://github.com/ARMmbed/mbed-os.git
+    # until it's not merged we use my branch
+    git clone --depth 1 https://github.com/ARMmbed/mbed-os.git -b feature-bluetooth-unit-test
+fi
--- tests/unittests.sh
+++ tests/unittests.sh
@@ -4,15 +4,8 @@ set -e
 # set pwd to script location
 cd "$( dirname "$0" )"
 
-# we need stubs from mbed-os
-if [ -d "mbed-os" ]
-then
-    echo "Using existing mbed-os"
-else
-    #git clone https://github.com/ARMmbed/mbed-os.git
-    # until it's not merged we use my branch
-    git clone --depth 1 https://github.com/ARMmbed/mbed-os.git -b feature-bluetooth-unit-test
-fi
+# Bootstrap of the environment 
+../bootstrap.sh
 
 cmake -S . -B cmake_build -GNinja -DCMAKE_BUILD_TYPE=Debug -DCOVERAGE:STRING=xml
 cmake --build cmake_build
@@ -23,5 +16,3 @@ cmake --build cmake_build
 (cd cmake_build; ctest -D ExperimentalMemCheck)
 # gcov (only show coverage of services)
 gcovr --html=coverage.html  -f ".*cmake_build/services.*"
-
-
