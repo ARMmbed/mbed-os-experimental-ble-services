5d79fc36863aba377c739eaa4cf961b71c2bd937
Abbas Bracken Ziad <abbas.brackenziad@arm.com>
2021-02-24 21:26:25 +0000

    Add symbolink links in bootstrap process
    * Add mbed-os to .gitignore

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,7 +12,12 @@ if [ -d "mbed-os" ]
 then
     echo "Using existing mbed-os"
 else
-    #git clone https://github.com/ARMmbed/mbed-os.git
+    # git clone https://github.com/ARMmbed/mbed-os.git
     # until it's not merged we use my branch
     git clone --depth 1 https://github.com/ARMmbed/mbed-os.git -b feature-bluetooth-unit-test
 fi
+
+# Add symlinks for Link Loss Service integration tests
+cd TESTS/LinkLoss/device
+ln -s ../../../mbed-os mbed-os
+ln -s ../../../../services/LinkLoss LinkLoss
