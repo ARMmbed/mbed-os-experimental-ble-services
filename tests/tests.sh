46f0209338a8265d9c61607dbd8d2fdb4add72ce
Abbas Bracken Ziad <abbas.brackenziad@arm.com>
2021-03-05 17:07:47 +0000

    Update compile targets

--- tests/tests.sh
+++ tests/tests.sh
@@ -7,6 +7,13 @@ cd "$( dirname "$0" )"
 # Bootstrap of the environment
 ../bootstrap.sh
 
-# Build Mbed apps
+# -- Link Loss Service --
 cd TESTS/LinkLoss/device
-mbed compile -t GCC_ARM -m K64F
+# Build for NRF52840_DK (old tools)
+mbed compile -t GCC_ARM -m NRF52840_DK
+# Build for NRF52840_DK (new tools)
+mbedtools compile -t GCC_ARM -m NRF52840_DK
+# Build for DISCO_L496AG (old tools)
+mbed compile -t GCC_ARM -m DISCO_L496AG
+# Build for DISCO_L496AG (new tools)
+mbedtools compile -t GCC_ARM -m DISCO_L496AG
