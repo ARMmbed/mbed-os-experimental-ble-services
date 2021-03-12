ac103f38a8dcde782f844b2fa9bda560a8026f07
Abbas Bracken Ziad <abbas.brackenziad@arm.com>
2021-03-12 11:51:20 +0000

    Consolidate CI workflow

--- activate.sh
+++ activate.sh
@@ -1,13 +1,31 @@
-ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+#!/bin/bash
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-# Test if the script is running on windows
+set -e
+
+# Enter repository root
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+# Test if script is running on windows
 windows() {
     [[ -n "$WINDIR" ]];
 }
 
 # Activate virtual environment
 if windows; then
-  source "$ROOT"/tests/TESTS/venv/Scripts/activate
+  source tests/TESTS/venv/Scripts/activate
 else
-  source "$ROOT"/tests/TESTS/venv/bin/activate
+  source tests/TESTS/venv/bin/activate
 fi
--- tests/unittests.sh
+++ tests/unittests.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 set -e
 
-# set pwd to script location
-cd "$( dirname "$0" )"
+# Enter repository root
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"/..
 
-# Bootstrap of the environment 
-../bootstrap.sh
+# Bootstrap of the environment
+./bootstrap.sh
+
+# Activate virtual environment
+source activate.sh
+
+cd tests
 
 cmake -S . -B cmake_build -GNinja -DCMAKE_BUILD_TYPE=Debug -DCOVERAGE:STRING=xml
 cmake --build cmake_build
 
-# normal test
+# Normal test
 (cd cmake_build; ctest -V)
 # valgrind
 (cd cmake_build; ctest -D ExperimentalMemCheck)
