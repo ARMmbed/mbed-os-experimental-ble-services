ad26776da142e16439768f571ce688163496b276
Abbas Bracken Ziad <abbas.brackenziad@arm.com>
2021-03-18 15:20:23 +0000

    Fix autocomplete bug
    Enabling `set -e` breaks bash-completion

--- activate.sh
+++ activate.sh
@@ -13,8 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -e
-
 # Enter repository root
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
